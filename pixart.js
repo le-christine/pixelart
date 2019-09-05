0130063ffd84bd3d8d0251518300e882016cd7d1
le-christine <christine.le244@gmail.com>
2019-09-05 18:02:16 -0400

    completed part 3 - makes 20 divs

--- pixart.js
+++ pixart.js
@@ -5,7 +5,7 @@
 document.querySelector('#set-color').addEventListener('click', changeBrushBox);
 
 /**
- * This function changes the brush box background color
+ * Changes the brush box background color
  * to whatever is in the input
  */
 function changeBrushBox (e) {
@@ -13,3 +13,17 @@ function changeBrushBox (e) {
   let newColor = document.querySelector('#color-field').value;
   document.querySelector('.brush').style.backgroundColor = newColor;
 }
+
+/**
+ * Creates twenty divs with square class and appends each to body
+ *
+ */
+function createTwentyDivs () {
+  for (let i=0; i<20; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList = "square";
+    document.querySelector('body').appendChild(newDiv);
+  }
+}
+
+createTwentyDivs();
