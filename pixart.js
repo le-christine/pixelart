6c379d8b21e3d190dcd1499bd0010686a50a6e8c
le-christine <christine.le244@gmail.com>
2019-09-05 17:56:31 -0400

    verified that function works with both click and enter events

--- pixart.js
+++ pixart.js
@@ -1,5 +1,13 @@
+/**
+ * This event listener will call the function changeBrushBox
+ * with 'click' and pressing the enter key.
+ */
 document.querySelector('#set-color').addEventListener('click', changeBrushBox);
 
+/**
+ * This function changes the brush box background color
+ * to whatever is in the input
+ */
 function changeBrushBox (e) {
   e.preventDefault();
   let newColor = document.querySelector('#color-field').value;
