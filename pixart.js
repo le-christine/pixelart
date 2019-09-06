1145978b5a5b16871cc5ca265e6d78bbeb0a1704
le-christine <christine.le244@gmail.com>
2019-09-05 20:52:08 -0400

    addedfunctional color swatches

--- pixart.js
+++ pixart.js
@@ -1,4 +1,4 @@
-/**
+/** part 1, part 2
  * This event listener will call the function changeBrushBox
  * with 'click' and pressing the enter key.
  */
@@ -14,7 +14,7 @@ function changeBrushBox (e) {
   document.querySelector('.brush').style.backgroundColor = newColor;
 }
 
-/**
+/** part 3 I would do createDivs(20);
  * Creates divs with square class and appends each to body
  * @param  {integer} [how many divs you want]
  */
@@ -26,9 +26,8 @@ function createDivs(num) {
   }
 }
 
-createDivs(8000);
 
-/**
+/** part 4
  * Adds an event listener to each square div
  */
 
@@ -52,6 +51,26 @@ function setGreen() {
 addEventListenerToSquares();
 */
 
+// Part 5
+/*
+function addEventListenerToSquares() {
+  squareObj = document.getElementsByClassName('square');
+  for (let i=0; i<squareObj.length; i++) {
+    squareObj[i].addEventListener('click', setSelectedColor);
+  }
+}
+
+function setSelectedColor() {
+  let newColor = document.querySelector('#color-field').value;
+  this.style.backgroundColor = newColor;
+}
+
+addEventListenerToSquares();
+*/
+
+// part 6
+createDivs(8000);
+
 function addEventListenerToSquares() {
   squareObj = document.getElementsByClassName('square');
   for (let i=0; i<squareObj.length; i++) {
@@ -65,3 +84,78 @@ function setSelectedColor() {
 }
 
 addEventListenerToSquares();
+
+/******************************************************************
+                              BONUS
+*******************************************************************/
+/**
+ * Makes a separate div to append to controls div.
+ * This will be where the swatches are
+ *
+ */
+function makeColorPalette () {
+  let colorPalette = document.createElement('div');
+  colorPalette.classList = "color-palette";
+  colorPalette.style.marginBottom = "15px";
+  document.querySelector('.controls').append(colorPalette);
+}
+
+/**
+ * Makes three div (swatches) to be appended to the color palette
+ *
+ */
+function makeColorSwatch () {
+  for (let i=1; i<4; i++) {
+  let newDiv = document.createElement('div');
+  newDiv.classList = `div-${i} color-swatch`;
+  newDiv.style.height = "33px";
+  newDiv.style.width = "33px";
+  newDiv.style.display ="inline-block";
+  newDiv.style.border = `1px solid white`;
+  newDiv.style.margin = '4px';
+  document.querySelector('.color-palette').appendChild(newDiv);
+  }
+}
+
+makeColorPalette();
+makeColorSwatch();
+
+let usedColors = ["transparent", "transparent", "transparent"];
+document.querySelector('#set-color').addEventListener('click', setPaletteColor);
+
+function setPaletteColor (e) {
+  e.preventDefault();
+  let colorAdded = document.querySelector('#color-field').value;
+  usedColors.unshift(colorAdded);
+  usedColors.pop();
+
+  let swatch1 = document.querySelectorAll(".color-swatch")[0];
+  let swatch2 = document.querySelectorAll(".color-swatch")[1];
+  let swatch3 = document.querySelectorAll(".color-swatch")[2];
+
+  swatch1.style.backgroundColor = usedColors[0];
+  swatch2.style.backgroundColor = usedColors[1];
+  swatch3.style.backgroundColor = usedColors[2];
+
+}
+
+for (let k=0; k<3; k++) {
+  let myColor = document.querySelectorAll(".color-swatch")[k];
+  myColor.addEventListener('click', brushPreview);
+}
+
+/**
+ * this will change the text of color field to background color of element
+ * thereby changing the color of brush preview
+ *
+ */
+
+function brushPreview(e) {
+  e.preventDefault();
+
+  let previewColor = document.getElementById('color-field');
+  previewColor.value = this.style.backgroundColor;
+
+  let brush = document.querySelector('.brush');
+  brush.style.background = previewColor.value;
+}
